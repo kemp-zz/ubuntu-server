7000c78d9e69e0455e40b31c4d81315b51470994
kemp-zz <fskemp@gmail.com>
2025-04-12 07:27:28 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-# Source ROS and the virtual environment
 
+# 激活ROS环境
 source /opt/ros/$ROS_DISTRO/setup.bash
-source /opt/ros2_ws/install/local_setup.bash
-source /root/myenv/bin/activate
 
-#Execute the command
+# 启动ROS桥接服务
+ros2 launch rosbridge_server rosbridge_websocket_launch.xml &
+
+# 启动JupyterLab
+jupyter-lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root &
+
+# 保持容器运行
 exec "$@"
